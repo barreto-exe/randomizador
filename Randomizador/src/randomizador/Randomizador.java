7f94f4f781d98864ece01a11b489b43f221ac0c1
Luis Barreto <luis.barreto.marin@gmail.com>
2020-05-18 22:04:08 -0400

    Ventana de inicio
    Creada interfaz para añadir participantes

---
+++ Randomizador/src/randomizador/Randomizador.java
@@ -0,0 +1,11 @@
+package randomizador;
+
+class Randomizador {
+
+    public static void main(String[] args) {
+        frmAgregarPersonas ventana = new frmAgregarPersonas();
+
+        ventana.setVisible(true);
+    }
+    
+}
